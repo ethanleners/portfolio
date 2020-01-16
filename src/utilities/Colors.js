aa61d107ae49a769b66d047290e7c7b53a2ea9f6
mbeche <mb.johnsonbece@gmail.com>
2020-01-15 18:25:00 -0600

    Add initial layout stuffs

---
+++ src/utilities/Colors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  background: "#191A2A", // dark blue
+  accent: "#FF4B5B", // red
+  secondary: "#FFD740", // yellow
+  text: "rgba(255, 255, 255, 0.87)",
+  overlay: "rgba(255, 255, 255, 0.12)",
+}
---
+++ src/utilities/index.js
@@ -0,0 +1,7 @@
+import elevation from './Elevation';
+
+export * from './Breakpoints';
+export * from './Positions';
+export * from './Colors';
+
+export { elevation };
