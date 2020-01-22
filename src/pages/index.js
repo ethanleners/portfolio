69cbb9f3cf814e83ed052d1422e142ca0bfb383d
Michael Leners <micleners@gmail.com>
2020-01-21 18:36:35 -0600

    Remove image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import EthanMain from "../components/ethanMain"
 import CaseStudy from "../components/caseStudy"
 import SEO from "../components/seo"
