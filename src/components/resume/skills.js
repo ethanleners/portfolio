7df3c81fbc1eb8515e7e21018806fd0c9409b968
ethanleners <59778851+ethanleners@users.noreply.github.com>
2020-02-20 15:20:43 -0600

    Update skills.js

--- src/components/resume/skills.js
+++ src/components/resume/skills.js
@@ -115,7 +115,7 @@ export const Skills = ({ children }) => {
         </FlexCard>
       </FlexRow>
       <DownloadResume
-        href="https://drive.google.com/open?id=1J3bIOzSgslYzoSsZJn4ky0oBIUuqQWH3"
+        href="https://drive.google.com/file/d/1LPzV66S2PrVM6a-5mSrHR7PeMT93b5L4/view?usp=sharing"
         target="_blank"
         rel="noopener norefferer"
       >
